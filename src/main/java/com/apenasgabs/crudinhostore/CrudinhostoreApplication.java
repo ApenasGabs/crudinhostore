3582ec01a0a54964ce3d22170c7005ec8b65ca51
Gabriel Rodrigues <gabers357@gmail.com>
2024-03-14 12:33:10 -0300

    Adiciona arquivos e configurações iniciais do projeto

---
+++ src/main/java/com/apenasgabs/crudinhostore/CrudinhostoreApplication.java
@@ -0,0 +1,13 @@
+package com.apenasgabs.crudinhostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudinhostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudinhostoreApplication.class, args);
+	}
+
+}
