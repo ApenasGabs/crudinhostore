9bf9d951874ce33efe69855bb830dd90e7f5cbdf
Gabriel Rodrigues <gabers357@gmail.com>
2024-03-14 15:43:13 -0300

    Adiciona método para deletar produto

--- src/main/java/com/apenasgabs/crudinhostore/controller/ProdutoController.java
+++ src/main/java/com/apenasgabs/crudinhostore/controller/ProdutoController.java
@@ -1,24 +1,28 @@
 package com.apenasgabs.crudinhostore.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.apenasgabs.crudinhostore.model.Produto;
 import com.apenasgabs.crudinhostore.repository.ProdutoRepository;
 
 import jakarta.validation.Valid;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/produto")
@@ -52,6 +56,15 @@ public class ProdutoController {
         .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
   }
 
-  
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+    Optional<Produto> produtoSelecionado = produtoRepository.findById(id);
+    if (produtoSelecionado.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    produtoRepository.deleteById(id);
+    return ResponseEntity.noContent().build();
+
+  }
 
 }
